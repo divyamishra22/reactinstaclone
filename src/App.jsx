3cbf224a4c1075129b9d5239926e653966777507
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-13 00:25:21 +0530

    update + router implemented

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,28 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from  './components/Navbar'
+import Home from './components/Home'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import Profile from './components/Profile'
+
 
 function App() {
   
 
   return (
     <>
+    <BrowserRouter>
     <Navbar/>
-      
+     <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/SignIn" element={<SignIn/>}></Route>
+      <Route path="/SignUp" element={<SignUp/>}></Route>
+      <Route path="/Profile" element={<Profile/>}></Route>
+      </Routes>
+       </BrowserRouter>
+
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import './Navbar.css'
-
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
     <div className='navbar'>
      <ul className='nav-menu'>
+      <Link to="SignUp">
       <li>SignUp</li>
+      </Link>
+
+      <Link to="SignIn">
       <li>SignIn</li>
+      </Link>
+
+      <Link to="Profile">
       <li>Profile</li>
+      </Link>
+      {/* <Link to="/">
+      <li>Home</li>
+      </Link> */}
      </ul>
     </div>
     </>
