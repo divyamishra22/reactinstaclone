66652dcd709c6cc354bdb659952eb19b1d87a751
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-12 15:42:14 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='cont'>
+     <ul className='nav-menu'>
+      <li>SignUp</li>
+      <li>SignIn</li>
+      <li>Profile</li>
+     </ul>
+    </div>
+  )
+}
+
+export default Navbar
