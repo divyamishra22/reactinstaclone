eb1879e1a5540d661afd9c1410bd87b0c558b957
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-13 12:40:17 +0530

    Login page implemented

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import './SignIn.css'
 const SignIn = () => {
   return (
-    <div className='class1'>
-        <p>Sign in to to view </p>
+    <div className='signin'>
+      <div>
+        
+        <div className='loginform'>
+          <div>
+          <input type="email" name="email" id="email" placeholder='Email'/>
+        </div>
+        <div>
+          <input type="password" name="password" id="password" placeholder='Password'/>
+        </div>
+        <button type='submit' id="login-btn" >LogIn</button>
+      </div>
       
+      <div className="loginform2">
+          Don't have an account ?
+          <Link to="/signup">
+            <span style={{ color: "blue", cursor: "pointer" }}>Sign Up</span>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
