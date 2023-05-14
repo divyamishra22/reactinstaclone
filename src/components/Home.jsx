b33d5e4b600a7bc617bac2d90e8e0a6c393a7349
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-14 22:46:20 +0530

    Home page implemented

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import './Home.css'
 const Home = () => {
   return (
-    <div>
-      
+    <div className='card'>
+      <div className='card-header'>
+        <div className='card-pic'>
+      <img src="./picture1.jpg"/>
+      </div>
+    <h5>Raha</h5>
     </div>
+    <div className='card-image'>
+      <img  src="./picture2.jpg"/>
+    </div>
+    <div className='card-content'>
+    <i class="fa-regular fa-heart"></i>
+    </div>
+    {/* <i class="fa-light fa-face-smile"></i> */}
+    <div className='add-comment'>
+    <input type='text' placeholder="Add Comment"/>
+    <button className="submit">Post</button>
+    </div>
+    </div>
+    
+   
   )
 }
 
