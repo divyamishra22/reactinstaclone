16d9cb85305ace1d3dd1331b752ac40e83f5e25f
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-14 23:23:20 +0530

    Profile page implemented

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-
+import React from 'react';
+import './Profile.css';
 const Profile = () => {
   return (
-    <div>
-      
+       <div className="profile">
+      {/* Profile frame */}
+      <div className="profile-frame">
+        {/* profile-pic */}
+        <div className="profile-pic">
+          <img src='picture1.jpg'/>
+        </div>
+        {/* profile-data */}
+        <div className="profile-data">
+          <h1>Raha Kapoor</h1>
+        <div className='profile-info' style={{display: 'flex', justifyContent: 'space-between'}}>
+          <p>40posts</p>
+          <p>40 followers</p>
+          <p>40 following</p>
+        </div>
+        </div>
+      </div>
+      <hr style={{ width: "90%", opacity: "0.8",margin: "25px auto",}}/>
+      {/* gallery */}
+      <div  className='gallery'>
+      <img src='picture1.jpg'/>
+      <img src='picture1.jpg'/>
+      <img src='picture1.jpg'/>
+      <img src='picture1.jpg'/>
+      <img src='picture1.jpg'/>
+      <img src='picture1.jpg'/>
+      </div>
     </div>
+    
   )
 }
 
