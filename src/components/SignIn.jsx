226b44b4a380839651af300f55612bfde2343d76
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-17 00:06:20 +0530

    jwt authentication for signin implemented

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,19 +1,63 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import { Link } from 'react-router-dom'
 import './SignIn.css'
+import axios from 'axios'
+
+
 const SignIn = () => {
+
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
+   const [isError, setIsError] = useState("");
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+        alert("Email or Password cannot be blank");
+    }
+    else {
+        getMyPostData();
+    }
+    setemail('');
+    setpassword('');
+};
+const getMyPostData = async () => {
+  try {
+    const res = await axios.post("http://localhost:3000/auth/login",{
+
+      password: password,
+      email:email,
+     
+    })
+   
+    
+    
+    console.log(res);
+    if(res.data === "user does not exist"){
+    alert(res.data);
+    }
+    else{
+      alert("SignedIn Successfully")
+    }
+  } catch (error) {
+    setIsError(error);
+  }
+}
   return (
     <div className='signin'>
       <div>
         
         <div className='loginform'>
           <div>
-          <input type="email" name="email" id="email" placeholder='Email'/>
+          <input type="email" name="email" id="email" value={email} placeholder='Email'
+           onChange={(e) => { setemail(e.target.value) }}/>
         </div>
         <div>
-          <input type="password" name="password" id="password" placeholder='Password'/>
+          <input type="password" name="password" id="password"   value={password} placeholder='Password'
+           onChange={(e) => { setpassword(e.target.value) }}/>
         </div>
-        <button type='submit' id="login-btn" >LogIn</button>
+        <button type='submit' id="login-btn"  onClick={handleSubmit} >LogIn</button>
       </div>
       
       <div className="loginform2">
