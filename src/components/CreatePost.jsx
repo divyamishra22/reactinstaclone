d8cddd83bd217638b06bb92d480e7fab940d425e
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-17 19:45:20 +0530

    update

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,23 +1,84 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import './CreatePost.css'
 
-const loadfile = (event) => {
-    var output = document.getElementById("output");
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function () {
-      URL.revokeObjectURL(output.src); // free memory
-    };
-  };
-
 const CreatePost = () => {
+
+  const [body, setBody] = useState("");
+  const [image, setImage] = useState("")
+  const [url, setUrl] = useState("")
+
+  const handleChange= (e)=> {
+    console.log(e.target.files);
+    setImage(e.target.files[0]);
+}
+  useEffect(() => {
+
+    // saving post to mongodb
+    if (url) {
+
+      fetch("http://localhost:3000/posts/upload", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + localStorage.getItem("jwt")
+        },
+        body: JSON.stringify({
+          post:body,
+          image: url
+        })
+      }).then(res => res.json())
+        .then(data => {
+          if (data.error) {
+            alert(data.error)
+          } else {
+            alert("Successfully Posted")
+            setBody(" ")
+            // navigate("/")
+          }
+        })
+        .catch(err => console.log(err))
+    }
+
+  }, [url])
+
+
+  const postDetails = () => {
+
+    console.log(body, image)
+    const data = new FormData()
+    data.append("file", image)
+    data.append("upload_preset", "myinstagram")
+    data.append("cloud_name", "divya123")
+    fetch("https://api.cloudinary.com/v1_1/divya123/image/upload", {
+      method: "post",
+      body: data
+    }).then(res => res.json())
+      .then(data => setUrl(data.url))
+      .catch(err => console.log(err))
+    console.log(url)
+
+  }
+
+  // const loadfile = (event) => {
+  //   var output = document.getElementById("output");
+  //   output.src = URL.createObjectURL(event.target.files[0]);
+  //   output.onload = function () {
+  //     URL.revokeObjectURL(output.src); // free memory
+  //   };
+  // };
+
   return (
     <div className='createpost'>
       <div className='post-header'>
         <h4 style={{margin: "3px auto"}}> Create a New Post</h4>
-        <button id='Share' >Share</button>
+        <button id='Share' onClick={() => { postDetails() }} >Share</button>
       </div>
       <div className='main-div'>
-        <input type='file' accept='image/*'   onChange={(event) => {loadfile(event)}}/>
+        <input type='file' accept='image/*' onChange={handleChange}
+        // onChange={(event) => {loadfile(event);
+            // setImage(event.target.files[0])
+            // }}
+            />
       </div>
       {/* details */}
       <div className="details">
@@ -28,7 +89,9 @@ const CreatePost = () => {
           </div>
           <h5>Raha</h5>
         </div>
-        <textarea  type="text" placeholder="Write a caption...."></textarea>
+        <textarea  type="text" placeholder="Write a caption...." value={body}
+        onChange={(e) => {
+          setBody(e.target.value)}}></textarea>
         </div>
     </div>
   )
