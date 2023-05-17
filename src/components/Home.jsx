2b4378e6191bb27dfe25a80cd9a650ab6194df12
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-18 02:27:10 +0530

    adding posts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,53 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Home.css'
 const Home = () => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwt");
+    // if (!token) {
+    //   navigate("./signup");
+    // }
+      // Fetching all posts
+      fetch("http://localhost:3000/posts/alluserposts", {
+        method: "get",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("jwt"),
+        },
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          console.log(result);
+          setData(result);
+        })
+        .catch((err) => console.log(err));
+    }, []);
+  
+
   return (
     <div className='card'>
-      <div className='card-header'>
-        <div className='card-pic'>
-      <img src="./picture1.jpg"/>
-      </div>
-    <h5>Raha</h5>
-    </div>
-    <div className='card-image'>
-      <img  src="./picture2.jpg"/>
-    </div>
-    <div className='card-content'>
-    <i className="fa-regular fa-heart"></i>
-    </div>
+      {data.map((posts) => {
+        return(
+      <><div className='card-header'>
+            <div className='card-pic'>
+              <img src="./picture2.jpg" />
+            </div>
+            <h5>Raha</h5>
+          </div><div className='card-image'>
+              <img src={posts.image} />
+            </div><div className='card-content'>
+              <i className="fa-regular fa-heart"></i>
+            </div>
     {/* <i class="fa-light fa-face-smile"></i> */}
     <div className='add-comment'>
     <input type='text' placeholder="Add Comment"/>
     <button className="submit">Post</button>
+    </div></>
+        )})}
     </div>
-    </div>
-    
-   
+        
+        
   )
 }
 
