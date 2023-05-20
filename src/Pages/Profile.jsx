ba457d96b63c972c5192ee156ad5bc48ca9b0f0a
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-20 22:25:11 +0530

    fetch profile of users by username

---
+++ src/Pages/Profile.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
+
+const Profile = () => {
+    const { username } = useParams();
+
+    const [user, setUser] = useState(null);
+    const [photos, setPhotos] = useState([]);
+    const [isFollow, setIsFollow] = useState(false);
+    const [isProfile, setIsProfile] = useState(false);
+    const [count, setCount] = useState(null);
+  
+
+    useEffect(()=>{
+    // const getProfile = () => {
+        try{
+            // setLoading(true);
+            fetch(`http://localhost:3000/user/viewprofile/${raha_K}`, {
+            method: "get",
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("jwt"),
+            },
+          })
+          .then((res) => res.json())
+            .then((result) => {
+              console.log(result);
+            //    setUser(result);
+            })
+       
+
+        }
+           catch (NotFoundException) {
+          console.log('User With Username does not Exist');
+        } 
+        // finally {
+        // //   setLoading(false);
+        // }
+    
+} , []);  
+
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+export default Profile
