3c5f8c382326c4662302b7a6cf9b7f1d01cbb60c
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-20 15:06:58 +0530

    search for user by username, name added

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,59 @@
+
+import React,{useState} from 'react';
+
+
+const Searchbar = () => {
+    const [term, setTerm] = useState('');
+    // const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!term) {
+            alert("Please insert all fields");
+        }
+        else {
+            searchAction();
+        }
+        setTerm('');
+    };
+
+    const searchAction = () => {
+        try{
+          if (term) {
+            setLoading(true);
+            fetch(`http://localhost:3000/user/search/${term}`, {
+            method: "get",
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("jwt"),
+            },
+          })
+          
+          .then((res) => res.json())
+            .then((result) => {
+              console.log(result);
+            //   setUsers(result);
+            })
+          }
+        }
+           catch (error) {
+          console.log(error.message);
+        } finally {
+          setLoading(false);
+        }
+    }   
+
+  return (
+    <div className='searchclass'>
+        <div className='searchinput'>
+
+        <input type="text" name="searchtext" id="searchtext" value={term} placeholder='Serach by Username..Name'
+           onChange={(e) => { setTerm(e.target.value) }}/>
+         <button type="submit" id="submit-btn" onClick={handleSubmit}>Search</button>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Searchbar
