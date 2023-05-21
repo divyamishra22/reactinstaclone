db9ed2609f6d1ff1fe708eaf81b75aaf4f996f1a
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-21 23:54:16 +0530

    fetch profile details on searching by username

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState , useMemo} from 'react'
 import {
     //  Link,
      useParams } from 'react-router-dom';
+     import api from '../api/index1';
 
 const Profile = () => {
     const { username } = useParams();
@@ -10,54 +11,49 @@ const Profile = () => {
     const [isFollow, setIsFollow] = useState(false);
     const [isProfile, setIsProfile] = useState(false);
     const [count, setCount] = useState(null);
+    // const [posts, setPosts] = useState([]);
   
    
-    // const  getProfile = async () => {
-     
-    // await  fetch(`http://localhost:3000/user/viewprofile/${username}`, {
-    //   method: "get",
-    //   headers: {
-    //     Authorization: "Bearer " + localStorage.getItem("jwt"),
-    //   },
-    // })
-    // .then((res) => res.json())
-    //   .then((result) => {
-    //     console.log(result);
-    //    setres(result);
-    //    console.log(res) 
-    //   })
-      
-  // }
+    useEffect(() => {
+      async function getProfile() {
+        const res = await api.get(`http://localhost:3000/user/viewprofile/${username}`);
+        console.log(res.data);
+        const {
+          isFollow: _isFollow,
+          isProfile: _isProfile,
+          userFollowersCount,
+          userFollowingCount,
+          userPostsCount,
+          user: _user,
+        } = res.data;
+        setIsFollow(_isFollow);
+        setCount({
+          userFollowersCount,
+          userFollowingCount,
+          userPostsCount,
+        });
+        setIsProfile(_isProfile);
+        setUser(_user);
+        // setPhotos(_user.photos);
+      }
+      getProfile();
+    }, [username]);
 
-    useEffect(()=>{
-  // getProfile(); 
-    fetch(`http://localhost:3000/user/viewprofile/${username}`, {
-      method: "get",
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("jwt"),
-      },
-    })
-    .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-        setUser(result.user);
-        console.log(user);
-      })
-     
-} , [username]);  
+    const loadingMemo = useMemo(() => !(user && user.id), [user]);
 
-
-            
+    if (loadingMemo) {
+      return <p>Loading..</p>;
+    }    
 
   return (
     <div className='profile'>
         <div className='profile-frame'>
             <div className='profile-pic'>
             {/* <img src={user.Photo ? user.Photo : picLink} alt="" /> */}
-            <img src="picture1.jpg"/>
+            {/* <img src="picture1.jpg"/> */}
             </div>
             <div className='profile-data'>
-            {/* <h1>{user.name}</h1> */}
+            <h1>{user.name}</h1>
             {/* {isProfile ? (
             //   <Link to={`/edit/${username}`}>
                 <button type='submit' id='editbtn'>Edit Profile</button>
@@ -69,9 +65,9 @@ const Profile = () => {
                 )} */}
             </div>
             <div className='profile-info'>
-            {/* <p>{count.userPostsCount} posts</p>
+            <p>{count.userPostsCount} posts</p>
             <p>{count.userFollowersCount} followers</p>
-            <p>{count.userFollowsCount} following</p> */}
+            <p>{count.userFollowsCount} following</p>
             </div>
         </div>
       
