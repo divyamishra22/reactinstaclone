4ceac3c99201f3bef5b2b20023a2c3c7bc0a30d0
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-23 02:24:05 +0530

    fetching feed , following of user by api  ,&& passing as props to profile component

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState , useMemo} from 'react'
+import Profile from '../components/Profile';
+
+
+const Home = () => {
+    
+    const [user,setUser] = useState('');
+    const [follow, setFollow] = useState('');
+    const [feed , setFeed]   = useState('');
+    
+    
+    useEffect(() => {
+    async function getfeed() {
+      fetch(`http://localhost:3000/feed`, {
+        method: "get",
+        headers: {
+          "Authorization": "Bearer " + localStorage.getItem("jwt")
+        },
+        })
+      .then(res => res.json())
+        .then(data => {
+         console.log(data)
+         setFeed(data);
+    })
+    }
+    async function getfollows() {
+        fetch(`http://localhost:3000/follow`,
+        {
+            method: "get",
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")   
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+         console.log(data)
+         setUser(data.user);
+         setFollow(data.follow);
+    })}
+    getfollows();
+    getfeed();
+  }, []);
+
+
+    const loadingMemo = useMemo(() => !(user && user.id), [user]);
+
+    if (loadingMemo) {
+      return <p>Loading..</p>;
+    }   
+
+
+  return (
+    <div>
+       {follow && 
+            follow.map((follows) => (
+        <Profile 
+                // img={user.avatar}
+                key={follows.userToId.id}
+                username={follows.userToId.username}
+                name={follows.userToId.name}/>
+            ) 
+           
+            )}
+    </div>
+  )
+}
+
+export default Home
