681173c4b1a724824719fbe1038b14c7f47caf57
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-25 23:48:28 +0530

    adding heart icons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import './Home.css'
 import Profile from '../components/Profile';
+// import { FaHeart} from 'react-icons/fa';
+// import { FiHeart } from 'react-icons/fi';
 
 const Main = ({ feed}) => {
 
   // const [data, setData] = useState([]);
   const [user, setUser] = useState(" ");
 
- 
- 
 
-    
-  
 
 
   return (
@@ -23,6 +21,14 @@ const Main = ({ feed}) => {
         />
          <div className="card-image">
          <img src={feed.posts.image} alt="" />
+         <div>
+         <i class="fa-regular fa-heart" size={18}
+            // style={{ color: '#fc4850', marginRight: 10 }}
+
+            >
+         </i>
+         <i class="fa-solid fa-heart"></i>
+         </div>
        </div>
     </div>
  </>        
