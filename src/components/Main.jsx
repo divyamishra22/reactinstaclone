98c8cba00085e61b805667a6c100e713e83a035f
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-25 23:18:49 +0530

    adding feed details

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import './Home.css'
-const Main = () => {
+import Profile from '../components/Profile';
 
-  const [data, setData] = useState([]);
-  const [name, setname] = useState(" ");
+const Main = ({ feed}) => {
 
+  // const [data, setData] = useState([]);
+  const [user, setUser] = useState(" ");
+
+ 
  
 
     
@@ -12,32 +15,17 @@ const Main = () => {
 
 
   return (
+    <>
     <div className='card'>
-      {data.map((posts) => {
-        return(
-      <><div className='card-header'>
-            <div className='card-pic'>
-              <img src="./picture2.jpg" />
-            </div>
-            <h5>{posts.name}</h5>
-          </div>
-          <div className='card-image'>
-              {/* <img src="./picture1.jpg" /> */}
-              <button className="submit">View Profile</button>
-            </div>
-            {/* <div className='card-content'>
-              <i className="fa-regular fa-heart"></i>
-            </div> */}
-    {/* <i class="fa-light fa-face-smile"></i> */}
-    {/* <div className='add-comment'>
-    <input type='text' placeholder="Add Comment"/>
-    <button className="submit">Post</button>
-    </div> */}
-    </>
-        )})}
+       <Profile
+          username={feed.posts.user.username}
+          name={feed.posts.user.name}
+        />
+         <div className="card-image">
+         <img src={feed.posts.image} alt="" />
+       </div>
     </div>
-        
-        
+ </>        
   )
 }
 
