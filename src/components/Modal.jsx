1dbb6363ef5dba7a6df532398acbf03f11908f8e
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-27 00:23:15 +0530

    func usefollow from context api & func to delete post

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useFollow } from '../hooks/follow';
+import { FiMoreHorizontal } from 'react-icons/fi';
 
 const Modal = ({isAuthor, post}) => {
+
+  const { Unfollow } = useFollow();
+
+  // let postid = post.id;
+
+
+  async function UnfollowUser(){
+    Unfollow(post.user.id);
+  }
+
+  async function DeletePost(){
+    fetch(`http://localhost:3000/posts/${post.id}`, {
+          method: "delete",
+          headers: {
+            "Authorization": "Bearer " + localStorage.getItem("jwt")
+          },
+          })
+        .then(res => res.json())
+          .then(data => {
+           console.log(data)})
+  }
+
+
   return (
     <>
+     <FiMoreHorizontal size={20} />
     <div>
        <div className="darkBg" >
       <div className="centered">
@@ -12,7 +38,7 @@ const Modal = ({isAuthor, post}) => {
               <Link to={`/photo/${post.id}`}>Go to Publication</Link>
             </li> */}
             <li className="red" 
-            // onClick={() => handleDelete(photo)}
+            onClick={DeletePost}
             >
               Delete Publication
             </li>
@@ -23,7 +49,7 @@ const Modal = ({isAuthor, post}) => {
               <Link to={`/photo/${post.id}`}>Go to Publication</Link>
             </li> */}
             <li className="red" 
-            // onClick={() => handleDelete(photo)}
+            onClick={UnfollowUser}
             >
             Unfollow
             </li>
