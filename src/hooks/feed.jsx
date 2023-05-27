2a6219b1511708b00dca835c966f3e6c1f6fd230
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-05-28 05:19:34 +0530

    func to get feed of logged in user

--- src/hooks/feed.jsx
+++ src/hooks/feed.jsx
@@ -5,4 +5,19 @@ const FeedContext = createContext();
 
 export function FeedProvider({ children }) {
   const [feeds, setFeeds] = useState([]);
+
+  async function getfeed() {
+    fetch(`http://localhost:3000/feed`, {
+      method: "get",
+      headers: {
+        "Authorization": "Bearer " + localStorage.getItem("jwt")
+      },
+      })
+    .then(res => res.json())
+      .then(data => {
+       console.log(data)
+       setFeed(data);
+       
+  })
+  }
 }
