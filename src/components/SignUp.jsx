79ff15af83e6ed0a92c8a204b205fcc5bf3445cd
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-06-01 12:16:50 +0530

    add name bio username

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,32 +4,38 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 const SignUp = () => {
   // const navigate = useNavigate()
-  // const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
   const [email, setemail] = useState("")
   const [password, setpassword] = useState("")
-  //  const [myData, setMyData] = useState([]);
+   const [name, setName] = useState("");
+   const [bio, setbio] = useState('');
    const [isError, setIsError] = useState("");
 
    const handleSubmit = (e) => {
     e.preventDefault();
-    if (!email || !password) {
-        alert("Email or Password cannot be blank");
+    if (!email || !password || !username || !name) {
+        alert("Cannot be blank");
     }
     else {
         getMyPostData();
     }
     setemail('');
     setpassword('');
+    setUsername('');
+    setName('');
+    setbio('');
+
 };
 
   
   const getMyPostData = async () => {
     try {
       const res = await axios.post("http://localhost:3000/user/signup",{
-
         password: password,
         email:email,
-       
+        username: username,
+        name: name,
+        bio: bio,
       })
      
       // setMyData(res.data);
@@ -39,26 +45,7 @@ const SignUp = () => {
       setIsError(error);
     }
 
-    // fetch("http://localhost:3000/user/signup", {
-    //   method: "post",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify({
-    //     email: email,
-    //     password: password
 
-    //   })
-    // }).then(res => res.json())
-    //    .then(data => {
-    //   // //   if (data.error) {
-    //   // //     notifyA(data.error)
-    //   // //   } else {
-    //   // //     notifyB(data.message)
-    //   // //     navigate("/signin")
-    //   // //   }
-    //      console.log(data)
-    //    })
   };
   return (
     <>
@@ -67,7 +54,12 @@ const SignUp = () => {
     {/* <div className='form'> */}
      <p> Sign In to view like follow your friends</p> 
     <div className='form0'>
-      <input type= "text" name="username" id="username" placeholder="Name"/>
+      <input type= "text" name="username" id="name" placeholder="Name"
+       onChange={(e) => { setName(e.target.value) }}/>
+    </div>
+    <div className='form4'>
+      <input type= "text" name="username" id="username" placeholder="Username"
+       onChange={(e) => { setUsername(e.target.value) }}/>
     </div>
     <div className='form1'>
       <input type= "email" name=" email" id="email" value={email} placeholder="Email"
@@ -76,6 +68,10 @@ const SignUp = () => {
     <div className='form2'>
       <input type= "password" name=" password" id="password"   value={password} placeholder="Password" 
        onChange={(e) => { setpassword(e.target.value) }}/>
+    </div>
+    <div className='form5'>
+      <input type= "text" name="username" id="bio" placeholder="Bio"
+       onChange={(e) => { setbio(e.target.value) }}/>
     </div>
     <p>By Signing Up, You Agree to our Terms and Policy</p>
     <button type="submit" id="submit-btn"  >SignUp</button>
