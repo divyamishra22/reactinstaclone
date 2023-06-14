c6a3e08e79c30435e2cf2e21487c5a881d53c2e5
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-06-14 20:51:31 +0530

    display profile pic if avatar present

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import './Profile.css'
 
 
-const Profile = ({username,name,con}) => {
+const Profile = ({img, username,name,con}) => {
+
+  
+ 
+
 
   return (
        <div className={con}>  
-       {/* <div> */}
-  <img src='https://raw.githubusercontent.com/OLucho/instagram-clone/master/frontend/src/assets/avatar.png'/>  
+       
+       {img?( <img src={img}/>):
+  (<img src='https://raw.githubusercontent.com/OLucho/instagram-clone/master/frontend/src/assets/avatar.png'/> )}
         <div>
-        <Link to={`/profile/${username}`}>
+          <div className='names'>
+        <Link to={`/profile/${username}`} >
         <div className='username'>
           {username}
           </div>
         </Link>
         <div className='name'>
           {name}
+        </div>
         </div>
       </div>
 
